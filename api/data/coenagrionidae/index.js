8e146f4c4f0d8b219b476955e7ab67744cb34de0
Jonas Myrenås <jonas@myrenas.se>
2019-10-20 17:42:17 +0200

    more Coenagrions

--- api/data/coenagrionidae/index.js
+++ api/data/coenagrionidae/index.js
@@ -4,6 +4,9 @@ const CoenagrionArmatum = require('./coenagrion-armatum.json')
 const CoenagrionCaerulescens = require('./coenagrion-caerulescens.json')
 const CoenagrionEcornutum = require('./coenagrion-ecornutum.json')
 const CoenagrionGlaciale = require('./coenagrion-glaciale.json')
+const CoenagrionHastulatum = require('./coenagrion-hastulatum.json')
+const CoenagrionHylas = require('./coenagrion-hylas.json')
+const CoenagrionIntermedium = require('./coenagrion-intermedium.json')
 
 module.exports = {
   ceriagrion: [CeriagrionGeorgifreyi, CeriagrionTenellum],
@@ -11,6 +14,9 @@ module.exports = {
     CoenagrionArmatum,
     CoenagrionCaerulescens,
     CoenagrionEcornutum,
-    CoenagrionGlaciale
+    CoenagrionGlaciale,
+    CoenagrionHastulatum,
+    CoenagrionHylas,
+    CoenagrionIntermedium
   ]
 }

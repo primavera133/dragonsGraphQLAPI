66e6e059d2f6d1a327bbd93dc4058e40ac0cf0fb
Jonas Myrenås <jonas@myrenas.se>
2020-09-17 17:42:48 +0200

    syntax fix

--- api/_context.js
+++ api/_context.js
@@ -4,7 +4,7 @@ const context = ({ req }) => {
   const authorizationHeader = (req.headers && req.headers.authorization) || ''
   const users = process.env.API_USERS || ''
   const usersArr = users.split(' ')
-  const token = authorizationHeader.replace('Bearer ')
+  const token = authorizationHeader.replace('Bearer ', '')
 
   if (!usersArr.includes(token)) {
     throw new ApolloError(`USER ${token} NOT FOUND`)

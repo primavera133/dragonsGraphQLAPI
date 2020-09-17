901bc93675b4e9e2f99e55c7d0236a4e82d28c54
Jonas Myrenås <jonas@myrenas.se>
2020-09-17 17:40:21 +0200

    conforming to Bearer standards

--- api/_context.js
+++ api/_context.js
@@ -1,9 +1,10 @@
 const { ApolloError } = require('apollo-server-micro')
 
 const context = ({ req }) => {
-  const token = (req.headers && req.headers.authorization) || ''
+  const authorizationHeader = (req.headers && req.headers.authorization) || ''
   const users = process.env.API_USERS || ''
   const usersArr = users.split(' ')
+  const token = authorizationHeader.replace('Bearer ')
 
   if (!usersArr.includes(token)) {
     throw new ApolloError(`USER ${token} NOT FOUND`)

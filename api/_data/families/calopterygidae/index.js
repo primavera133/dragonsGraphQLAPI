cf714a7743fb043770821c912418b2418aa7a511
Jonas Myrenås <jonas.myrenas@mindler.se>
2022-06-05 22:15:09 +0200

    C. syriaca

--- api/_data/families/calopterygidae/index.js
+++ api/_data/families/calopterygidae/index.js
@@ -2,6 +2,7 @@ const CalopteryxExul = require('./calopteryx/calopteryx-exul.json')
 const CalopteryxHaemorrhoidalis = require('./calopteryx/calopteryx-haemorrhoidalis.json')
 const CalopteryxHyalina = require('./calopteryx/calopteryx-hyalina.json')
 const CalopteryxSplendens = require('./calopteryx/calopteryx-splendens.json')
+const CalopteryxSyriaca = require('./calopteryx/calopteryx-syriaca.json')
 const CalopteryxVirgo = require('./calopteryx/calopteryx-virgo.json')
 const CalopteryxXanthostoma = require('./calopteryx/calopteryx-xanthostoma.json')
 
@@ -11,6 +12,7 @@ module.exports = {
     CalopteryxHaemorrhoidalis,
     CalopteryxHyalina,
     CalopteryxSplendens,
+    CalopteryxSyriaca,
     CalopteryxVirgo,
     CalopteryxXanthostoma
   ]

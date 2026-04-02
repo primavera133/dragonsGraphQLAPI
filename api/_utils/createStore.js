974caf35050c97115191b9d3762de543c0fe9e9d
Jonas Myrenås <jonas.myrenas@mindler.se>
2026-04-02 09:13:40 +0200

    zod schema

--- api/_utils/createStore.js
+++ api/_utils/createStore.js
@@ -4,15 +4,24 @@ const allFamilies = require('./allFamilies')
 const allGenera = require('./allGenera')
 const genera = require('../_data/genera')
 const families = require('../_data/families')
+const { z } = require('zod')
+const { SpecieSchema, TaxonInfoSchema } = require('../_schemas')
 
-module.exports.createSpeciesStore = () => ({
-  species,
-  allSpecies: allSpecies(species),
-  allFamilies: allFamilies(species),
-  allGenera: allGenera(species)
-})
+module.exports.createSpeciesStore = () => {
+  const allSpeciesArr = allSpecies(species)
+  z.array(SpecieSchema).parse(allSpeciesArr)
 
-module.exports.createAboutStore = () => ({
-  genera,
-  families
-})
+  return {
+    species,
+    allSpecies: allSpeciesArr,
+    allFamilies: allFamilies(species),
+    allGenera: allGenera(species),
+  }
+}
+
+module.exports.createAboutStore = () => {
+  Object.values(families).forEach(family => TaxonInfoSchema.parse(family))
+  Object.values(genera).forEach(genus => TaxonInfoSchema.parse(genus))
+
+  return { genera, families }
+}
